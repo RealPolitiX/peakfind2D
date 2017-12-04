8c456b10aa7de841c3fe1e4803e477d5fa20e9d4
R. Patrick Xian <xrpatrick@gmail.com>
2017-12-04 10:54:33 +0100

    minor text fixes

--- peakfind2D/distanceFilter.m
+++ peakfind2D/distanceFilter.m
@@ -5,11 +5,11 @@ function svmax = distanceFilter(imgmat, maxmat, dist)
     for s = 1:npks
 
         % Calculate proximity metric vector (each element is the squared
-        % cartesian distance between the brightest pixels in the chosen blocks
+        % Cartesian distance between the brightest pixels in the chosen blocks)
         proxvec = (maxmat(:,1) - maxmat(s,1)).^2 + (maxmat(:,2) - maxmat(s,2)).^2;
         prind = find(proxvec < dist^2,1);
 
-        % Proximity elimination: if there are peaks very close by, the compare
+        % Proximity elimination: if there are peaks very close by, then compare
         % the values of the two peaks and retain the biggest
         if prind(1) ~= s && imgmat(maxmat(s,1),maxmat(s,2)) > imgmat(maxmat(prind(1),1),maxmat(prind(1),2))
             maxmat(prind(1),:) = [0 0 0];
